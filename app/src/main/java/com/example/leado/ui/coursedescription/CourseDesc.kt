1e2f04341a41de9e261bdd2574d32c54c13374ec
Etch <mostafah2000@live.com>
2020-07-27 20:47:00 +0200

    - Course Description design done

---
+++ app/src/main/java/com/example/leado/ui/coursedescription/CourseDesc.kt
@@ -0,0 +1,11 @@
+package com.example.leado.ui.coursedescription
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseDesc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_desc)
+    }
+}
